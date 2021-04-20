cc27b228d99d40897500429396b4172ed2e18c67
Kriss Jessop <kriss@k-jessop.co.uk>
2021-04-20 20:38:58 +0100

    Add project files.

---
+++ Flights.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Flights.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
